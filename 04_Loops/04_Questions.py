34cb4048b2b705cabd7a29f9958f581f6c4d69f2
Iterative-Muse <iterativeMuse@gmail.com>
2024-08-01 03:05:48 +0530

    Questions

--- 04_Loops/04_Questions.py
+++ 04_Loops/04_Questions.py
@@ -104,12 +104,13 @@ Output = 120
 # print(f"Factorial is {prod}.")
 
 #Q53. Ask to numbers x and y from the user. If x<y then print all the numbers from x to y, but if y<x then print all the numbers from y to x.
-x = int(input("Enter the value of x = "))
-y = int(input("Enter the value of y = "))
-
-if x < y:
-    
-    for x in range(x,y)
-
-
-
+# a = x = int(input("Enter the value of x = "))
+# b = y = int(input("Enter the value of y = "))
+# a += 1
+# b += 1
+# if x < y:
+#     for x in range(x,b):
+#         print(x,end=(" "))
+# else:
+#     for y in range(y,a):
+#         print(y,end=(" "))
--- 04_Loops/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-num = 0
-while i <= 200:
-    if i % 6 == 0 or i % 7 == 0:
-        print(i,end=" ")
-        num += 1
-    i += 1
-print(f"\nTher

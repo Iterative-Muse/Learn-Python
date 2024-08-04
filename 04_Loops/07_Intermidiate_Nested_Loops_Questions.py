7a4e19f7a67f2682b35cca990817260f875bf1c9
Iterative-Muse <iterativeMuse@gmail.com>
2024-08-04 23:18:14 +0530

    Update 07_Intermidiate_Nested_Loops_Questions.py

--- 04_Loops/07_Intermidiate_Nested_Loops_Questions.py
+++ 04_Loops/07_Intermidiate_Nested_Loops_Questions.py
@@ -137,3 +137,74 @@ WAP to print the following pattern
 #     for j in range (i,0,-1):
 #         print(j,end=(" "))
 #     print()
+
+"""
+WAP to print the following pattern 
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+"""
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(i,end=" ")
+#     print()
+
+"""
+WAP to print the following pattern 
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print("*",end=" ")
+#     print()
+
+"""
+WAP to print the following pattern 
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+"""
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=(" "))
+#     print()
+
+"""
+WAP to print the following pattern 
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+"""
+# sum = 1
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(sum,end=" ")
+#         sum = sum+1
+#     print()
+
+
+"""
+WAP to print the following pattern 
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+"""
+
+for i in range (1,6):
+    for j in range (i,0,-1):
+        print(j,end=(" "))
+    print()

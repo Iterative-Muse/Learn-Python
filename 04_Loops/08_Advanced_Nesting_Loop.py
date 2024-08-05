b3f75ca6f61d02ba9d63cd19b6f13dc343725e13
Iterative-Muse <iterativeMuse@gmail.com>
2024-08-06 00:08:42 +0530

    Update 08_Advanced_Nesting_Loop.py

--- 04_Loops/08_Advanced_Nesting_Loop.py
+++ 04_Loops/08_Advanced_Nesting_Loop.py
@@ -7,12 +7,12 @@ WAP a program to print below pattern
 * * * * *
 """
 
-# for i in range (1,6):
-#     for j in range (i,5):
-#         print(" ",end=(" "))
-#     for k in range(1,i+1):
-#         print("*",end=" ")
-#     print()
+for i in range (1,6):
+    for j in range (i,5):
+        print(" ",end=(" "))
+    for k in range(1,i+1):
+        print("*",end=" ")
+    print()
 
 
 """
@@ -55,9 +55,9 @@ WAP a program to print below pattern
 * * * * * * * * *
 """
 
-# for i in range(1,6):
-#     for j in range(i,5):
-#         print(" ",end=" ")
-#     for k in range(1,(i*2)):
-#         print("*",end =(" "))
-#     print()
+for i in range(1,6):
+    for j in range(i,5):
+        print(" ",end=" ")
+    for k in range(1,(i*2)):
+        print("*",end =(" "))
+    print()

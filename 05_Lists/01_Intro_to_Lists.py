f8dfea52c357c55347d3c74cec7e1becd0d86df7
Iterative-Muse <iterativeMuse@gmail.com>
2024-08-10 11:07:51 +0530

    Create 01_Intro_to_Lists.py

---
+++ 05_Lists/01_Intro_to_Lists.py
@@ -0,0 +1,18 @@
+"""
+Lists : To store multiple value in single variable 
+
+We can add any type of data in the list eg. 44, 65.78, "Tushar", False, true
+"""
+
+tushar = [45, 67, 74, 83, 92]
+print(tushar)
+print(type(tushar))
+
+
+Abc = [44, 65.78, "Tushar", False, True]
+print(Abc)
+print(type(Abc))
+
+"""
+Accessing element from List (Indexing / Positioning)
+"""
